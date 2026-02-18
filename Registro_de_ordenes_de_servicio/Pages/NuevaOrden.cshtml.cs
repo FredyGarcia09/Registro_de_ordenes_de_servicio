0a992640192bbbf5e4ab705cc4c9a7f0984e5297
FredyGarcia09 <s23120180@alumnos.itsur.edu.mx>
2026-02-17 22:42:28 -0600

    Se creo el diseño de las paginas para registrar orden y el index

---
+++ Registro_de_ordenes_de_servicio/Pages/NuevaOrden.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Registro_de_ordenes_de_servicio.Pages
+{
+    public class NuevaOrdenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
